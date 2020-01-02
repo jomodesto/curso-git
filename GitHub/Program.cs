53280c17b2c184ee1f3a22c17f5a5012b5656374
Jonatas Modesto <modesto.jonatas18@gmail.com>
2020-01-02 20:20:23 -0300

    add a frase Olá tudo beem?

---
+++ GitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste 1");
+            Console.WriteLine("Olá tudo beem?");
+
+
+        }
+    }
+}
